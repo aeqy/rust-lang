e54552925c63b93324ef555bc5f505e46ce3e698
eqycc <a@eqy.cc>
2025-05-17 18:52:59 +0800

    feat: 入门

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ::std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("请输入你的名字");
+    
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    
+    println!("你好,{}欢迎入门Rust Lang 程序!", input);
+    
 }
